37d481cfcbadd63991682ffcdbbc732c246c105c
Muhammad-javed440 <meherjaved440@gmail.com>
2024-03-23 22:48:00 +0600

    first comment

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+// 1-compter will generate a random number
+// 2-user input for guessing number
+// 3-compare user input with computer gentating number and show resulte
+const randomNumber = Math.floor(Math.random() * (5 - 1) + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 5  = "
+    }
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Congratulation! you won the game.");
+}
+else {
+    console.log("You lose the game,Try again");
+}
